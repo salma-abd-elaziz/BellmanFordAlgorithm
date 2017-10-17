61f7b2488039d27d6f25057531a6d9f081f07f43
salma-abd-elaziz <salma-abd-elaziz@users.noreply.github.com>
2017-10-17 22:27:41 +0200

    first code commit

---
+++ src/crack/BellmanFordImplemenation.java
@@ -0,0 +1,54 @@
+package crack;
+
+public class BellmanFordImplemenation {
+
+	/*
+	 * finds the shortest path in a graph using bellmanford Algorithm.
+	 * 
+	 * @param graph the adjacency list of the graph.
+	 * 
+	 * @param weights is the array that contains the weights of the edges.
+	 * 
+	 * @return array of the shortest distance from the src to each vertices.
+	 */
+	public int[] findMinimunPathBF(int src, int[][] graph) {
+		// The distance from the src to each vertices.
+		int[] dist = new int[graph.length];
+		// Maximizing the distance of the sre to all the vertices.
+		for (int i = 0; i < dist.length; i++) {
+			dist[i] = Integer.MAX_VALUE;
+		}
+		dist[src] = 0;
+
+		
+			// For each edge in the graph. 
+			for (int i = 0; i < graph.length; i++) {
+				for (int j = 0; j < graph.length; j++) {
+					// Relax edge(i, j).
+					if (graph[i][j] != 0) {
+						if (dist[i] != Integer.MAX_VALUE
+								&& dist[j] > dist[i] + graph[i][j]) {
+							dist[j] = dist[i] + graph[i][j];
+						}
+					}
+				}
+			}
+		
+		// Check if there is a negative cycle, loop the edges.
+		for (int i = 0; i < graph.length; i++) {
+			// For each edge in the graph.
+			for (int j = 0; j < graph.length; j++) {
+				if (graph[i][j] != 0) {
+					// Still can relax the edges?.
+					if (dist[i] != Integer.MAX_VALUE
+							&& dist[j] > dist[i] + graph[i][j]) {
+						// Put a sign of having a negative cycle.
+						dist[0] = Integer.MIN_VALUE;
+						return dist;
+					}
+				}
+			}
+		}
+		return dist;
+	}
+}
